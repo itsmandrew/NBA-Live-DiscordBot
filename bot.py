93b7b08e35ebc94da1e24077535321211af930c0
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2023-12-16 19:50:07 -0600

    Finished daily command

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
+from daily_script import display_top_players, group_players_by_matchup, fetch_player_game_logs, build_table_for_matchup
 import discord
 import json
-
+import datetime
+from table2ascii import table2ascii as t2a, PresetStyle
 
 # Define the path to your JSON file
 file_path = 'creds.json'  # Replace with your actual file path
@@ -19,5 +21,35 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print("Testing")
+    channel = bot.get_channel(CHANNEL_ID)
+
+    await channel.send("Salmaan sucks at fantasy")
+
+@bot.command() 
+async def stats(ctx):
+    
+    await ctx.send("Checking stats")
+
+
+@bot.command()
+async def daily(ctx):
+    yesterday_str = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+    season = "2023-24"
+
+    player_stats = fetch_player_game_logs(yesterday_str, yesterday_str, season)
+    matchup_player_stats, game_id_to_matchup = group_players_by_matchup(player_stats)
+
+    # Creating a Discord Embed
+    embed = discord.Embed(title="NBA Fantasy Points Summary", color=discord.Color.blue())
+
+    for game_id, players in matchup_player_stats.items():
+        formatted_matchup = game_id_to_matchup[game_id]  # Fetch the matchup string
+        table_str = build_table_for_matchup(formatted_matchup, players)
+        embed.add_field(name=f"**{formatted_matchup} ({game_id})**", value=table_str, inline=False)
+
+    if not embed.fields:
+        embed.description = "No data available for the specified date."
+
+    await ctx.send(embed=embed)
 
 bot.run(BOT_TOKEN)
--- daily_script.py
+++ daily_script.py
@@ -1,35 +1,76 @@
-from nba_api.stats.endpoints import playergamelogs
 import datetime
+from nba_api.stats.endpoints import playergamelogs
+from table2ascii import table2ascii as t2a, PresetStyle
 
-# Fetch player game logs for the specified date range
-try:
-    player_logs = playergamelogs.PlayerGameLogs(
-        date_from_nullable="12/15/2023",
-        date_to_nullable="12/15/2023",
-        season_nullable="2023-24"
+
+def fetch_player_game_logs(date_from, date_to, season):
+    try:
+        player_logs = playergamelogs.PlayerGameLogs(
+            date_from_nullable=date_from,
+            date_to_nullable=date_to,
+            season_nullable=season
+        )
+        return player_logs.get_normalized_dict()['PlayerGameLogs']
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return []
+
+def group_players_by_matchup(player_stats):
+    game_id_to_matchup = {}  # Stores the matchup name for each game ID
+    matchup_player_stats = {}  # Stores player stats grouped by game ID
+
+    for entry in player_stats:
+        game_id = entry['GAME_ID']
+        matchup = entry['MATCHUP'].replace(' @ ', ' vs ')
+        player_name = entry['PLAYER_NAME']
+        fantasy_points = entry['NBA_FANTASY_PTS']
+
+        # Store the matchup name for each game ID
+        game_id_to_matchup[game_id] = matchup
+
+        # Group player stats by game ID
+        if game_id not in matchup_player_stats:
+            matchup_player_stats[game_id] = []
+        matchup_player_stats[game_id].append((player_name, fantasy_points))
+
+    return matchup_player_stats, game_id_to_matchup
+
+
+def display_top_players(matchup_player_stats, game_id_to_matchup, top_n=5):
+    for game_id, players in matchup_player_stats.items():
+        formatted_matchup = game_id_to_matchup[game_id]
+        top_players = sorted(players, key=lambda x: x[1], reverse=True)[:top_n]
+
+        print(f"Matchup {formatted_matchup} ({game_id}) -------------------------- :")
+        for player, score in top_players:
+            print(f"   -{player}: {score} fantasy points")
+        print()
+
+
+
+def build_table_for_matchup(matchup, players, top_n=5):
+    top_players = sorted(players, key=lambda x: x[1], reverse=True)[:top_n]
+    table_content = [(player, f"{score:.1f}") for player, score in top_players]
+    table = t2a(
+        header=["Player", "Fantasy Points"],
+        body=table_content,
+        style=PresetStyle.thin_compact
     )
-    player_stats = player_logs.get_normalized_dict()['PlayerGameLogs']
-except Exception as e:
-    print(f"Error fetching data: {e}")
-    player_stats = []
+    return f"```{table}```"
 
-# Group players by matchup
-matchup_player_stats = {}
-for entry in player_stats:
-    matchup = entry['MATCHUP']
-    player_name = entry['PLAYER_NAME']
-    fantasy_points = entry['NBA_FANTASY_PTS']
 
-    if matchup not in matchup_player_stats:
-        matchup_player_stats[matchup] = []
-    matchup_player_stats[matchup].append((player_name, fantasy_points))
 
-# Sort and get top 5 players for each matchup
-for matchup, players in matchup_player_stats.items():
-    # Replace '@' with 'vs'
-    formatted_matchup = matchup.replace(' @ ', ' vs ')
-    top_players = sorted(players, key=lambda x: x[1], reverse=True)[:5]
-    print(f"Matchup {formatted_matchup} -------------------------- :")
-    for player, score in top_players:
-        print(f"   -{player}: {score} fantasy points")
-    print()
+
+# Main execution
+if __name__ == "__main__":
+    yesterday_str = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%m/%d/%Y")
+    season = "2023-24"
+
+    # Fetch player game logs
+    player_stats = fetch_player_game_logs(yesterday_str, yesterday_str, season)
+
+    # Group players by game_id and also get the mapping of game_id to matchup names
+    matchup_player_stats, game_id_to_matchup = group_players_by_matchup(player_stats)
+
+    # Display the top players for each game, along with the matchup names
+    display_top_players(matchup_player_stats, game_id_to_matchup, top_n=5)
